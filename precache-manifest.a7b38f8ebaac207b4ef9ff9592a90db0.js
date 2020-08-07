766b632470a32324ff155145cf6b7c72d9c9290f
Steffi Joel <steffijerome0809>
2020-08-07 17:48:50 -0400

    Updates

---
+++ precache-manifest.a7b38f8ebaac207b4ef9ff9592a90db0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ebd8689916a222dc7a655872403da1",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "2a6ebd4bcd0eb7961798",
+    "url": "/Employee-Directory/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "05d17aef3855cdba9734",
+    "url": "/Employee-Directory/static/css/main.667fb86f.chunk.css"
+  },
+  {
+    "revision": "2a6ebd4bcd0eb7961798",
+    "url": "/Employee-Directory/static/js/2.24c76813.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.24c76813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d17aef3855cdba9734",
+    "url": "/Employee-Directory/static/js/main.62735b66.chunk.js"
+  },
+  {
+    "revision": "21ab4dbde2f8e2bfdf72",
+    "url": "/Employee-Directory/static/js/runtime-main.965eecad.js"
+  }
+]);
